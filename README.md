ff814bc8c495fa6019a2d23711e38a23c221a9e3
r3drun3 <simone.ragonesi@sighup.io>
2024-02-07 18:07:23 +0100

    feat: first iteration
    Signed-off-by: r3drun3 <simone.ragonesi@sighup.io>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# CNCF Landscape Ranking
+# CNCF Landscape Ranking 🖼
+
 
 Ranking of CNCF landscape projects via *Criticality Score*.  
 
@@ -7,5 +8,5 @@ Ranking of CNCF landscape projects via *Criticality Score*.
 This repo contains a go script to rank all the [CNCF projects](https://github.com/cncf/landscape) using [this](https://github.com/engelsjk/criticalityscore) porting of the [criticality score](https://github.com/ossf/criticality_score).  
 
 
-## Ranking
-Below the table with the rankings  
+## Rankings
+<table><tr><th>Rank</th><th>Name</th><th>Score</th><th>Scored On</th></tr><tr><td>1</td><td><a href="https://github.com/ansible/ansible">ansible</a></td><td>0.702920</td><td>Wed Feb  7 16:58:43 2024</td></tr><tr><td>2</td><td><a href="https://github.com/chef/chef">chef</a></td><td>0.608080</td><td>Wed Feb  7 16:59:02 2024</td></tr><tr><td>3</td><td><a href="https://github.com/cloudfoundry/bosh">bosh</a></td><td>0.577370</td><td>Wed Feb  7 16:58:49 2024</td></tr><tr><td>4</td><td><a href="https://github.com/cloud-custodian/cloud-custodian">cloud-custodian</a></td><td>0.566550</td><td>Wed Feb  7 16:59:33 2024</td></tr><tr><td>5</td><td><a href="https://github.com/cfengine/core">core</a></td><td>0.538510</td><td>Wed Feb  7 16:58:59 2024</td></tr><tr><td>6</td><td><a href="https://github.com/uber/cadence">cadence</a></td><td>0.507740</td><td>Wed Feb  7 16:58:53 2024</td></tr><tr><td>7</td><td><a href="https://github.com/cdk8s-team/cdk8s">cdk8s</a></td><td>0.482340</td><td>Wed Feb  7 16:58:56 2024</td></tr><tr><td>8</td><td><a href="https://github.com/apolloconfig/apollo">apollo</a></td><td>0.476700</td><td>Wed Feb  7 16:58:46 2024</td></tr><tr><td>9</td><td><a href="https://github.com/project-akri/akri">akri</a></td><td>0.358650</td><td>Wed Feb  7 16:58:40 2024</td></tr><tr><td>10</td><td><a href="https://github.com/airshipit/treasuremap">treasuremap</a></td><td>0.266210</td><td>Wed Feb  7 16:58:37 2024</td></tr></table>
--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/engelsjk/criticalityscore/criticalityscore"
 	"gopkg.in/yaml.v2"
@@ -31,8 +33,21 @@ type Item struct {
 	RepoURL string `yaml:"repo_url"`
 }
 
+type RepoScore struct {
+	Name             string
+	URL              string
+	CriticalityScore float64
+	ScoredOn         time.Time
+}
+
+type ByCriticalityScore []RepoScore
+
+func (a ByCriticalityScore) Len() int           { return len(a) }
+func (a ByCriticalityScore) Less(i, j int) bool { return a[i].CriticalityScore > a[j].CriticalityScore }
+func (a ByCriticalityScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
 func main() {
-	// Retrieve YAML data from the CNCF landscape
+	fmt.Println("RANKSCAPE\nRetrieving CNCF landscape projects...")
 	resp, err := http.Get("https://raw.githubusercontent.com/cncf/landscape/master/landscape.yml")
 	if err != nil {
 		log.Fatal("Error retrieving landscape YAML:", err)
@@ -63,17 +78,20 @@ func main() {
 		}
 	}
 
-	// Calculate criticality score for each GitHub repository
+	// Calculate and collect criticality score for each GitHub repository
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
 	if token == "" {
 		fmt.Println("warning: env variable GITHUB_AUTH_TOKEN not provided")
 	}
 
-	//for _, repoURL := range githubRepos {
+	var repoScores []RepoScore
+	fmt.Println("Calculating scores for CNCF landscape projects...")
 	for i, repoURL := range githubRepos {
+		// just for testing in order to avoid reaching rete limit on github api
 		if i >= 10 {
-			break // Just for test: Exit the loop after the 10th element
+			break
 		}
+		fmt.Printf("Project %d of %d\n", i+1, len(githubRepos))
 		repo, err := criticalityscore.LoadRepository(repoURL, token)
 		if err != nil {
 			fmt.Printf("Error loading repository %s: %s\n", repoURL, err)
@@ -86,7 +104,51 @@ func main() {
 			continue
 		}
 
-		// Print GitHub repo URL along with criticality score
-		fmt.Printf("Repository: %s, Criticality Score: %f\n", repoURL, score)
+		// Parse ScoredOn string to time.Time
+		scoredOn, err := time.Parse("Mon Jan 2 15:04:05 MST 2006", score.ScoredOn)
+		if err != nil {
+			fmt.Printf("Error parsing ScoredOn for repository %s: %s\n", repoURL, err)
+			continue
+		}
+
+		repoScores = append(repoScores, RepoScore{
+			Name:             score.Name,
+			URL:              repoURL,
+			CriticalityScore: score.CriticalityScore,
+			ScoredOn:         scoredOn,
+		})
+	}
+
+	// Sort repositories by criticality score
+	sort.Sort(ByCriticalityScore(repoScores))
+
+	// Generate HTML table
+	htmlTable := "<table><tr><th>Rank</th><th>Name</th><th>Score</th><th>Scored On</th></tr>"
+	for i, repoScore := range repoScores {
+		rank := i + 1
+		htmlTable += fmt.Sprintf("<tr><td>%d</td><td><a href=\"%s\">%s</a></td><td>%f</td><td>%s</td></tr>",
+			rank, repoScore.URL, repoScore.Name, repoScore.CriticalityScore, repoScore.ScoredOn.Format("Mon Jan _2 15:04:05 2006"))
+	}
+	htmlTable += "</table>"
+
+	// Update README.md with the generated HTML table
+	updateReadme(htmlTable)
+	fmt.Println("Rankings updates successfully")
+}
+
+func updateReadme(htmlTable string) {
+	// Read the existing content of README.md
+	readmeFile, err := os.ReadFile("README.md")
+	if err != nil {
+		log.Fatal("Error reading README.md:", err)
+	}
+
+	// Replace the placeholder with the generated HTML table
+	readmeContent := strings.Replace(string(readmeFile), "<!--TABLE_PLACEHOLDER-->", htmlTable, 1)
+
+	// Write the updated content back to README.md
+	err = os.WriteFile("README.md", []byte(readmeContent), 0644)
+	if err != nil {
+		log.Fatal("Error writing README.md:", err)
 	}
 }
